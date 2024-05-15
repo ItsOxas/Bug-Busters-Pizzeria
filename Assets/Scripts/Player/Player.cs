2d2398a3e509460fad49dc29b950f0de8c7316e2
ItsOxas <121440551+ItsOxas@users.noreply.github.com>
2024-05-15 19:37:53 +0300

    Player(alpha ver)

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    float speedX;
+    float speedY;
+    Vector2 moveDirection;
+
+    Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        speedX = Input.GetAxis("Horizontal") * speed;
+        speedY = Input.GetAxis("Vertical") * speed;
+
+        moveDirection = rb.velocity = new Vector2(speedX, speedY);
+
+        if (moveDirection != Vector2.zero)
+        {
+            float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+    }
+}
