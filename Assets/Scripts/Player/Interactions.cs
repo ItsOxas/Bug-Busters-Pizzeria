4ec69a0a860ee8a6bd451925fd3858ae2e17737f
Lo-oked <146217089+Lo-oked@users.noreply.github.com>
2024-05-16 19:31:45 +0300

    bandymas1

---
+++ Assets/Scripts/Player/Interactions.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactions : MonoBehaviour
+{
+    
+    void Start()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+
+        }
+    }
+
+    
+    void Update()
+    {
+        
+    }
+     
+    
+}
