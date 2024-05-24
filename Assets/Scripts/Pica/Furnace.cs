9eabf21c783a9f534e7a3fd6d06f17f520a681a1
Lo-oked <146217089+Lo-oked@users.noreply.github.com>
2024-05-24 17:33:28 +0300

    FurnaceButtons
    Patobulinau scripta

---
+++ Assets/Scripts/Pica/Furnace.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Furnace : MonoBehaviour
+{
+
+    public float TimeToMakePizza = 5.0f;
+    public bool makepizza = false;
+    public List<GameObject> Picos;
+    public int KuriPica;
+    public GameObject AllButtons;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (makepizza)
+        {
+            if (TimeToMakePizza > 0.0f)
+            {
+                TimeToMakePizza -= Time.deltaTime;
+            }
+            else if (TimeToMakePizza < 0.0f)
+            {
+                PizzaReady();
+                makepizza = false;
+            }
+        }
+    }
+
+    public void ChosingPizza()
+    {
+        AllButtons.SetActive(true);
+    }
+
+    public void MakingPizza()
+    {
+        KuriPica = gameObject.GetComponent<PicosButtons>().KuriPica;
+        makepizza = true;
+        TimeToMakePizza = Picos[KuriPica].GetComponent<Pica>().TimeForPicaToMake;
+        AllButtons.SetActive(false);
+    }
+    public void PizzaReady()
+    {
+        print("hello");
+        Instantiate(Picos[KuriPica]);
+        
+    }
+}
---
+++ Assets/Scripts/Pica/Pica.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Pica : MonoBehaviour
+{
+    
+
+    public int WitchPica;
+    public int TimeForPicaToMake;
+    public int MoneyEarnFromPica;
+    GameObject player;
+
+    public void CarryingPizza()
+    {
+        print(":(");
+        player = GameObject.FindWithTag("Player");
+        transform.parent = player.transform;
+    }
+}
